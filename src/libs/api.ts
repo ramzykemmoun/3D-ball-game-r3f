b76129e81deafa02e524b6a96119d69b4b03f415
ramzy1453 <kemmounramzy93@gmail.com>
2024-11-30 19:08:03 +0100

    api integration

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,5 @@
 export async function createUser(username: string) {
-  const response = await fetch("http://localhost:8080/player", {
+  const response = await fetch(process.env.VITE_API_URL! + "/player", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,20 +11,23 @@ export async function createUser(username: string) {
 }
 
 export async function updateScore(userId: string, score: number) {
-  const response = await fetch(`http://localhost:8080/player/score/${userId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ Score: score + 0.02 }),
-  });
+  const response = await fetch(
+    process.env.VITE_API_URL! + `/player/score/${userId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ Score: score + 0.02 }),
+    }
+  );
 
   const { data } = await response.json();
   return data.player.MatchedCount > 0;
 }
 
 export async function getLeaderboard() {
-  const response = await fetch("http://localhost:8080/leaderboard");
+  const response = await fetch(process.env.VITE_API_URL! + "/leaderboard");
   const { data } = await response.json();
   if (data.results) {
     return data.results;
