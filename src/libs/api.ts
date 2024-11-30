a860b569a3a1d4cc93e983ba5e1339a9329f6fe9
ramzy1453 <kemmounramzy93@gmail.com>
2024-11-30 19:12:10 +0100

    fixed process env

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,7 @@
+const baseUrl = import.meta.env.VITE_API_URL!;
+
 export async function createUser(username: string) {
-  const response = await fetch(process.env.VITE_API_URL! + "/player", {
+  const response = await fetch(baseUrl + "/player", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,23 +13,20 @@ export async function createUser(username: string) {
 }
 
 export async function updateScore(userId: string, score: number) {
-  const response = await fetch(
-    process.env.VITE_API_URL! + `/player/score/${userId}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ Score: score + 0.02 }),
-    }
-  );
+  const response = await fetch(baseUrl + `/player/score/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ Score: score + 0.02 }),
+  });
 
   const { data } = await response.json();
   return data.player.MatchedCount > 0;
 }
 
 export async function getLeaderboard() {
-  const response = await fetch(process.env.VITE_API_URL! + "/leaderboard");
+  const response = await fetch(baseUrl + "/leaderboard");
   const { data } = await response.json();
   if (data.results) {
     return data.results;
